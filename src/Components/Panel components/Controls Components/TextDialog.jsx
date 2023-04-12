7e6a5a618b281c9b275892f52045bb25b7df9210
khaledhajij <100922947+khaledhajij@users.noreply.github.com>
2023-04-12 12:51:54 +0300

    Adding random text property

--- src/Components/Panel components/Controls Components/TextDialog.jsx
+++ src/Components/Panel components/Controls Components/TextDialog.jsx
@@ -2,10 +2,33 @@ import React, { useState } from 'react'
 import { Button } from 'primereact/button'
 import { Dialog } from 'primereact/dialog'
 import { InputTextarea } from 'primereact/inputtextarea'
+import { InputText } from 'primereact/inputtext'
+import { nanoid } from 'nanoid'
+import { Checkbox } from 'primereact/checkbox'
 
 const TextDialog = props => {
   const [visible, setVisible] = useState(false)
+  const [randomText, setRandomText] = useState('')
+  const [randomTextArray, setRandomTextArray] = useState([])
+  const [addingRandomText, setAddingRandomText] = useState(false)
 
+  const handleRandomTextDelete = props => {
+    setRandomTextArray(preArr => preArr.filter(txt => txt.id !== props.id))
+  }
+
+  const randomTextList = randomTextArray.map(e => (
+    <div className='random-text-list-item'>
+      <p>{e.text}</p>
+      <div className='check-delete'>
+        <Button
+          icon='pi pi-trash'
+          className='p-button-danger'
+          onClick={() => handleRandomTextDelete(e)}
+        />
+        <Checkbox />
+      </div>
+    </div>
+  ))
   const handleAccept = () => {
     setVisible(false)
     props.setShownText(props.text)
@@ -14,6 +37,15 @@ const TextDialog = props => {
   const handleDecline = () => {
     setVisible(false)
     props.setText('')
+  }
+
+  const handleRandomTextAccept = e => {
+    setAddingRandomText(false)
+    setRandomTextArray(preArr => [
+      ...preArr,
+      { text: randomText, id: nanoid() }
+    ])
+    setRandomText('')
   }
 
   return (
@@ -54,6 +86,28 @@ const TextDialog = props => {
           autoResize
           style={{ width: '100%' }}
         />
+        <div className='add-new-text'>
+          <Button icon='pi pi-plus' onClick={() => setAddingRandomText(true)} />
+          {addingRandomText && (
+            <>
+              <InputText
+                value={randomText}
+                onChange={e => setRandomText(e.target.value)}
+              />
+              <Button
+                icon='pi pi-check'
+                className='p-button-success'
+                onClick={handleRandomTextAccept}
+              />
+              <Button
+                icon='pi pi-trash'
+                className='p-button-danger'
+                onClick={() => setAddingRandomText(false)}
+              />
+            </>
+          )}
+        </div>
+        <ul>{randomTextList}</ul>
       </Dialog>
     </div>
   )
